1900c7fd19c7576620401381f6cff0a7d148e345
quannguyen2724 <quannguyen2724@gmail.com>
2020-12-23 17:21:43 +0700

    no message

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 
-export const API_ADDR = process.env.REACT_APP_API_ADDR || 'http://52.15.163.233:8080';
-export const WS_ADDR = process.env.REACT_APP_WS_ADDR || '52.15.163.233:8080' ;
+export const API_ADDR = process.env.REACT_APP_API_ADDR || 'http://localhost:8080';
+export const WS_ADDR = process.env.REACT_APP_WS_ADDR || 'localhost:8080';
