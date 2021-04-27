c53879a1f470a9445de8f21fe05dc821501e5cb1
Toan Nguyen <blackntt@gmail.com>
2021-04-27 15:13:25 +0700

    fix: re transaction list

--- src/components/home/block.js
+++ src/components/home/block.js
@@ -1,88 +1,117 @@
-import React from 'react';
-import { List } from 'antd';
-import { BlockOutlined } from '@ant-design/icons';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { connect } from 'react-redux';
-import { loadRecentBlocks } from '../../actions/home';
-import { Row, Col } from 'antd';
+import React from "react";
+import { List } from "antd";
+import { BlockOutlined } from "@ant-design/icons";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import { connect } from "react-redux";
+import { loadRecentBlocks } from "../../actions/home";
+import { Row, Col } from "antd";
 import {
-	RecentListTitleFrame,
-	RecentListTitle,
-	RecentListContentFrame,
-	RecentItem,
-	RecentItemData, StyledLink,
-	RecentRightCol,
-} from './recent-list';
-import {toTimeAgo} from '../../utils/utils';
+  RecentListTitleFrame,
+  RecentListTitle,
+  RecentListContentFrame,
+  RecentItem,
+  RecentItemData,
+  StyledLink,
+  RecentRightCol,
+} from "./recent-list";
+import { toTimeAgo } from "../../utils/utils";
 
 class BlockList extends React.Component {
-	componentDidMount() {
-		this.props.loadRecentBlocks();
-	}
-	blockItem = (block) => {
-		return <List.Item key={block.num}>
-			<RecentItem>
-				<Row >
-				<Col xs={24} sm={24} md={12}>
-						<span>Block:<RecentItemData><StyledLink to={`/block/${block.num}`}>{block.num}</StyledLink></RecentItemData></span>
-					</Col>
-					<RecentRightCol xs={24}  sm={24} md={12} >
-						<span>Producer:<RecentItemData ><StyledLink to={`/account/${block.witness_address}`}>{block.witness_name?block.witness_name:block.witness_address}</StyledLink></RecentItemData></span>
-					</RecentRightCol>
-				</Row>
-				<Row >
-					<Col span={24}>
-						<span>Include: <RecentItemData>{block.num_of_txs}</RecentItemData> transactions</span>
-					</Col>
-				</Row>
-				<Row >
-					<Col xs={24}  sm={24} md={12}>
-						<span>Block reward:<RecentItemData>{16}</RecentItemData></span>
-					</Col>
-					<RecentRightCol xs={24}  sm={24} md={12}>
-						<span><RecentItemData>{block.timestamp?toTimeAgo(block.timestamp):"unknown"}</RecentItemData></span>
-					</RecentRightCol>
-				</Row>
-			</RecentItem>
-		</List.Item>;
-	}
-	render() {
-		let blocks = this.props.blocks;
-		blocks.sort(function (a, b) { return b.num - a.num });
-		return (
-			<div>
-				<RecentListTitleFrame>
-					<BlockOutlined />
-					<RecentListTitle>Recent Blocks</RecentListTitle>
-				</RecentListTitleFrame>
-				<RecentListContentFrame>
-					<PerfectScrollbar>
-						<List
-							dataSource={blocks}
-							loading={blocks.length === 0 ? true:false}
-							locale={{ emptyText: 'Loading' }}
-							renderItem={block => (
-								this.blockItem(block)
-							)}
-						/>
-					</PerfectScrollbar>
-				</RecentListContentFrame>
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.props.loadRecentBlocks();
+  }
+  blockItem = (block) => {
+    return (
+      <List.Item key={block.num}>
+        <RecentItem>
+          <Row>
+            <Col xs={24} sm={24} md={12}>
+              <span>
+                Block:
+                <RecentItemData>
+                  <StyledLink to={`/block/${block.num}`}>
+                    {block.num}
+                  </StyledLink>
+                </RecentItemData>
+              </span>
+            </Col>
+            <RecentRightCol xs={24} sm={24} md={12}>
+              <span>
+                Producer:
+                <RecentItemData>
+                  <StyledLink to={`/account/${block.witness_address}`}>
+                    {block.witness_name
+                      ? block.witness_name
+                      : block.witness_address}
+                  </StyledLink>
+                </RecentItemData>
+              </span>
+            </RecentRightCol>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <span>
+                Include: <RecentItemData>{block.num_of_txs}</RecentItemData>{" "}
+                transactions
+              </span>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24} sm={24} md={12}>
+              <span>
+                Block reward:<RecentItemData>{16}</RecentItemData>
+              </span>
+            </Col>
+            <RecentRightCol xs={24} sm={24} md={12}>
+              <span>
+                <RecentItemData>
+                  {block.timestamp ? toTimeAgo(block.timestamp) : "unknown"}
+                </RecentItemData>
+              </span>
+            </RecentRightCol>
+          </Row>
+        </RecentItem>
+      </List.Item>
+    );
+  };
+  render() {
+    let blocks = this.props.blocks ?? [];
+    blocks.sort(function (a, b) {
+      return b.num - a.num;
+    });
+    return (
+      <div>
+        <RecentListTitleFrame>
+          <BlockOutlined />
+          <RecentListTitle>Recent Blocks</RecentListTitle>
+        </RecentListTitleFrame>
+        <RecentListContentFrame>
+          <PerfectScrollbar>
+            <List
+              dataSource={blocks}
+              loading={blocks.length === 0 ? true : false}
+              locale={{ emptyText: "Loading" }}
+              renderItem={(block) => this.blockItem(block)}
+            />
+          </PerfectScrollbar>
+        </RecentListContentFrame>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		blocks: state.homeBlocks.blocks,
-	};
+  return {
+    blocks: state.homeBlocks.blocks,
+  };
 };
-const mapDispatchToProps = dispatch => {
-	return {
-		loadRecentBlocks: () => {
-			dispatch(loadRecentBlocks());
-		},
-
-	};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadRecentBlocks: () => {
+      dispatch(loadRecentBlocks());
+    },
+  };
 };
-export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(BlockList);
+export default connect(mapStateToProps, mapDispatchToProps, null, {
+  forwardRef: true,
+})(BlockList);
--- src/components/home/transaction.js
+++ src/components/home/transaction.js
@@ -1,121 +1,166 @@
-import React from 'react';
-import { List } from 'antd';
-import { BlockOutlined } from '@ant-design/icons';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { connect } from 'react-redux';
-import { loadRecentTrans } from '../../actions/home';
-import { Row, Col } from 'antd';
+import React from "react";
+import { List } from "antd";
+import { BlockOutlined } from "@ant-design/icons";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import { connect } from "react-redux";
+import { loadRecentTrans } from "../../actions/home";
+import { Row, Col } from "antd";
 import {
-	RecentListTitleFrame,
-	RecentListTitle,
-	RecentListContentFrame,
-	RecentItem,
-	RecentItemData, StyledLink,
-	RecentRightCol,
-} from './recent-list';
-import {toTimeAgo,currencyFormat, decimalFormat} from '../../utils/utils';
+  RecentListTitleFrame,
+  RecentListTitle,
+  RecentListContentFrame,
+  RecentItem,
+  RecentItemData,
+  StyledLink,
+  RecentRightCol,
+} from "./recent-list";
+import { toTimeAgo, currencyFormat, decimalFormat } from "../../utils/utils";
 
 class TransactionList extends React.Component {
+  componentDidMount() {
+    this.props.loadRecentTrans();
+  }
+  tranItem = (tran) => {
+    var amount = null;
+    const contractType = tran.contract.type;
+    switch (contractType) {
+      default:
+        amount = null;
+        break;
+      case "TransferContract":
+        amount =
+          currencyFormat(
+            decimalFormat(tran.contract.parameter.raw.amount / 1000000)
+          ) + " ACG";
+        break;
+      case "TransferAssetContract":
+        amount = currencyFormat(tran.contract.parameter.raw.amount);
+        break;
+    }
+    return (
+      <List.Item key={tran.hash}>
+        <RecentItem>
+          <Row>
+            <Col xs={24} sm={24} md={12}>
+              <span>
+                Transaction:
+                <RecentItemData>
+                  <StyledLink to={`/transaction/${tran.hash}`} target="_blank">
+                    {tran.hash.substring(0, 24) + "..."}
+                  </StyledLink>
+                </RecentItemData>
+              </span>
+            </Col>
+            <RecentRightCol xs={24} sm={24} md={12}>
+              <span>
+                <RecentItemData>
+                  {amount}&nbsp;
+                  {tran.contract.type === "TransferAssetContract" ? (
+                    <StyledLink
+                      to={`/token/${tran.contract.parameter.raw.AssetID}`}
+                      target="_blank"
+                    >
+                      {tran.contract.parameter.raw.AssetName}
+                    </StyledLink>
+                  ) : null}
+                </RecentItemData>
+              </span>
+            </RecentRightCol>
+          </Row>
+          <Row>
+            <Col xs={0} sm={0} md={24}>
+              {tran.contract.type === "TransferAssetContract" ||
+              (tran.contract.type === "TransferContract" &&
+                tran.contract.parameter.raw.owner_address &&
+                tran.contract.parameter.raw.to_address) ? (
+                <div>
+                  <span>From </span>
+                  <StyledLink
+                    to={"/account/" + tran.contract.parameter.raw.owner_address}
+                    target="_blank"
+                  >
+                    {tran.contract.parameter.raw.owner_address.substring(0, 7) +
+                      "..." +
+                      tran.contract.parameter.raw.owner_address.substring(
+                        tran.contract.parameter.raw.owner_address.length - 4,
+                        tran.contract.parameter.raw.owner_address.length - 1
+                      )}
+                  </StyledLink>
+                  <span> To </span>
+                  <StyledLink
+                    to={"/account/" + tran.contract.parameter.raw.to_address}
+                    target="_blank"
+                  >
+                    {tran.contract.parameter.raw.to_address.substring(0, 7) +
+                      "..." +
+                      tran.contract.parameter.raw.to_address.substring(
+                        tran.contract.parameter.raw.to_address.length - 4,
+                        tran.contract.parameter.raw.to_address.length - 1
+                      )}
+                  </StyledLink>
+                </div>
+              ) : (
+                <br></br>
+              )}
+            </Col>
+          </Row>
 
-	componentDidMount() {
-		this.props.loadRecentTrans();
-	}
-	tranItem = (tran) => {
-		var amount = null;
-		const contractType = tran.contract.type;
-		switch(contractType){
-			default: amount= null;break;
-			case "TransferContract": amount = currencyFormat(decimalFormat(tran.contract.parameter.raw.amount/1000000)) + " ACG";break;
-			case "TransferAssetContract": amount = currencyFormat(tran.contract.parameter.raw.amount);break;
-		}
-		return <List.Item key={tran.hash}>
-			<RecentItem>
-				<Row >
-					<Col xs={24} sm={24} md={12} >
-						<span>Transaction:<RecentItemData>
-							<StyledLink to={`/transaction/${tran.hash}`} target="_blank">{tran.hash.substring(0, 24) + "..."}</StyledLink>
-						</RecentItemData></span>
-					</Col>
-					<RecentRightCol xs={24} sm={24} md={12} >
-						<span>
-							<RecentItemData>
-								{amount}&nbsp;
-								{	tran.contract.type==="TransferAssetContract"?
-									<StyledLink to={`/token/${tran.contract.parameter.raw.AssetID}`} target="_blank">
-										{tran.contract.parameter.raw.AssetName}
-									</StyledLink>
-									:null}
-							</RecentItemData>
-						</span>
-					</RecentRightCol>
-				</Row>
-				<Row >
-					<Col xs={0} sm={0} md={24}>
-						{tran.contract.type==="TransferAssetContract"||tran.contract.type==="TransferContract" && tran.contract.parameter.raw.owner_address&&tran.contract.parameter.raw.to_address?
-						<div>
-							<span>From </span>
-							<StyledLink to={"/account/"+tran.contract.parameter.raw.owner_address} target="_blank">
-								{tran.contract.parameter.raw.owner_address.substring(0,7)+
-									"..."+tran.contract.parameter.raw.owner_address.substring(tran.contract.parameter.raw.owner_address.length-4,tran.contract.parameter.raw.owner_address.length-1)}
-							</StyledLink>
-							<span> To </span>
-							<StyledLink to={"/account/"+tran.contract.parameter.raw.to_address} target="_blank">
-								{tran.contract.parameter.raw.to_address.substring(0,7)+
-									"..."+tran.contract.parameter.raw.to_address.substring(tran.contract.parameter.raw.to_address.length-4,tran.contract.parameter.raw.to_address.length-1)}
-							</StyledLink>
-						</div> 
-						:<br></br>}
-					</Col>
-				</Row>
-
-				<Row >
-					<Col xs={24} sm={24} md={12}>
-						<span>Type:<RecentItemData>{tran.contract.type}</RecentItemData></span>
-					</Col>
-					<RecentRightCol xs={24} sm={24} md={12} >
-						<span><RecentItemData>{tran.timestamp?toTimeAgo(tran.timestamp):"unknown"}</RecentItemData></span>
-					</RecentRightCol>
-				</Row>
-			</RecentItem>
-		</List.Item>;
-	}
-	render() {
-		let trans = this.props.trans;
-		trans.sort(function (a, b) { return b.timestamp - a.timestamp });
-		return (
-			<div>
-				<RecentListTitleFrame>
-					<BlockOutlined />
-					<RecentListTitle>Recent Transactions</RecentListTitle>
-				</RecentListTitleFrame>
-				<RecentListContentFrame>
-					<PerfectScrollbar>
-						<List
-							dataSource={trans}
-							renderItem={tran => (
-								this.tranItem(tran)
-							)}
-							loading={trans.length === 0 ? true:false}
-							locale={{ emptyText: 'Loading' }}
-						/>
-					</PerfectScrollbar>
-				</RecentListContentFrame>
-			</div>
-		);
-	}
+          <Row>
+            <Col xs={24} sm={24} md={12}>
+              <span>
+                Type:<RecentItemData>{tran.contract.type}</RecentItemData>
+              </span>
+            </Col>
+            <RecentRightCol xs={24} sm={24} md={12}>
+              <span>
+                <RecentItemData>
+                  {tran.timestamp ? toTimeAgo(tran.timestamp) : "unknown"}
+                </RecentItemData>
+              </span>
+            </RecentRightCol>
+          </Row>
+        </RecentItem>
+      </List.Item>
+    );
+  };
+  render() {
+    let trans = this.props.trans ?? [];
+    trans.sort(function (a, b) {
+      return b.timestamp - a.timestamp;
+    });
+    return (
+      <div>
+        <RecentListTitleFrame>
+          <BlockOutlined />
+          <RecentListTitle>Recent Transactions</RecentListTitle>
+        </RecentListTitleFrame>
+        <RecentListContentFrame>
+          <PerfectScrollbar>
+            <List
+              dataSource={trans}
+              renderItem={(tran) => this.tranItem(tran)}
+              loading={trans.length === 0 ? true : false}
+              locale={{ emptyText: "Loading" }}
+            />
+          </PerfectScrollbar>
+        </RecentListContentFrame>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		trans: state.homeTrans.trans,
-	};
+  return {
+    trans: state.homeTrans.trans,
+  };
 };
-const mapDispatchToProps = dispatch => {
-	return {
-		loadRecentTrans: () => {
-			dispatch(loadRecentTrans());
-		},
-
-	};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadRecentTrans: () => {
+      dispatch(loadRecentTrans());
+    },
+  };
 };
-export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(TransactionList);
+export default connect(mapStateToProps, mapDispatchToProps, null, {
+  forwardRef: true,
+})(TransactionList);
