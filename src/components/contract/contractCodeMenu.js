48eb52eb7f31f5249eaa7d3c53346b119d58b12d
superda10 <68041104+superda10@users.noreply.github.com>
2021-06-27 16:01:48 +0700

    fix: change default tab contract detail (#37)

--- src/components/contract/contractCodeMenu.js
+++ src/components/contract/contractCodeMenu.js
@@ -1,106 +1,139 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import {Tabs, Select, notification} from 'antd';
-import ContractCode from './contractCode';
-import ReadContract from './readContract';
-import WriteContract from './writeContract';
-import {Wrapper, Div} from './style';
-import {updatePriKey} from '../../actions/contract';
-import styled from 'styled-components';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Tabs, Select, notification } from 'antd'
+import ContractCode from './contractCode'
+import ReadContract from './readContract'
+import WriteContract from './writeContract'
+import { Wrapper, Div } from './style'
+import { updatePriKey } from '../../actions/contract'
+import styled from 'styled-components'
 
-const { Option } = Select;
+const { Option } = Select
 
 const StyleSelect = styled(Select)`
-	width:200px;
-`;
+  width: 200px;
+`
 
-class ContractCodeMenu extends Component{
-	componentDidMount(){
-		var {prikeys, login} = this.props;
-		if(!prikeys.prikeys||prikeys.prikeys.length===0){
-            notification.warning({
-                message: 'Warning!',
-                description: "You have no private key, please add somes in private key management to perform transaction!",
-            });
-        }
-	}
+class ContractCodeMenu extends Component {
+  componentDidUpdate() {
+    var { prikeys } = this.props
+    if (!prikeys.prikeys || prikeys.prikeys.length === 0) {
+      notification.warning({
+        message: 'Warning!',
+        description:
+          'You have no private key, please add somes in private key management to perform transaction!',
+      })
+    }
+  }
 
-	onChange = (value)=>{
-		this.props.updatePriKey(value);
-	}
+  onChange = (value) => {
+    this.props.updatePriKey(value)
+  }
 
-	render(){
-		let {contract, prikeys} = this.props;
-		var noRead = 0;
-		var noWrite = 0;
-		var read = contract.abi?contract.abi.map((func, index)=>{
-			if(!func.stateMutability||func.stateMutability===0||func.stateMutability===1||func.stateMutability===2){
-				noRead++;
-				return <ReadContract key={index} func = {func} no={noRead} addr = {contract.contract_address}/>;
-			}
-			return null;
-		}):null;
-		var write = contract.abi?contract.abi.map((func, index)=>{
-			if(func.stateMutability===4||func.stateMutability===3){
-				noWrite++;
-				return <WriteContract key={index} func = {func} no={noWrite} addr = {contract.contract_address}/>;
-			}
-			return null;
-		}):null;
-		return (
-			<Wrapper>
-				<Div>
-			        <Tabs defaultActiveKey="1" type="card">
-					    <Tabs.TabPane tab="Code" key="1">
-					        <ContractCode/>
-					    </Tabs.TabPane>
-					    <Tabs.TabPane tab="Read Contract" key="2">
-					    	<Wrapper>
-					    		<StyleSelect
-                                    showSearch
-                                    placeholder="Select a private key"
-                                    allowClear
-                                    onChange={this.onChange}
-                                >
-                                    {prikeys.prikeys&&prikeys.prikeys.length!==0?prikeys.prikeys.map((value, index) => <Option value={value.prikey} key={index}>{value.name}</Option>):null}
-                                </StyleSelect>
-					    	</Wrapper>
-					        {read}
-					    </Tabs.TabPane>
-					    <Tabs.TabPane tab="Write Contract" key="3">
-					    	<Wrapper>
-					    		<StyleSelect
-                                    showSearch
-                                    placeholder="Select a private key"
-                                    allowClear
-                                    onChange={this.onChange}
-                                >
-                                    {prikeys.prikeys&&prikeys.prikeys.length!==0?prikeys.prikeys.map((value, index) => <Option value={value.prikey} key={index}>{value.name}</Option>):null}
-                                </StyleSelect>
-					    	</Wrapper>
-					        {write}
-					    </Tabs.TabPane>
-					</Tabs>
-				</Div>
-			</Wrapper>
-		);
-	}
+  render() {
+    let { contract, prikeys } = this.props
+    var noRead = 0
+    var noWrite = 0
+    var read = contract.abi
+      ? contract.abi.map((func, index) => {
+          if (
+            !func.stateMutability ||
+            func.stateMutability === 0 ||
+            func.stateMutability === 1 ||
+            func.stateMutability === 2
+          ) {
+            noRead++
+            return (
+              <ReadContract key={index} func={func} no={noRead} addr={contract.contract_address} />
+            )
+          }
+          return null
+        })
+      : null
+    var write = contract.abi
+      ? contract.abi.map((func, index) => {
+          if (func.stateMutability === 4 || func.stateMutability === 3) {
+            noWrite++
+            return (
+              <WriteContract
+                key={index}
+                func={func}
+                no={noWrite}
+                addr={contract.contract_address}
+              />
+            )
+          }
+          return null
+        })
+      : null
+    return (
+      <Wrapper>
+        <Div>
+          <Tabs defaultActiveKey="1" type="card">
+            <Tabs.TabPane tab="Code" key="1">
+              <ContractCode />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Read Contract" key="2">
+              <Wrapper>
+                <StyleSelect
+                  showSearch
+                  placeholder="Select a private key"
+                  allowClear
+                  onChange={this.onChange}
+                >
+                  {prikeys.prikeys && prikeys.prikeys.length !== 0
+                    ? prikeys.prikeys.map((value, index) => (
+                        <Option value={value.prikey} key={index}>
+                          {value.name}
+                        </Option>
+                      ))
+                    : null}
+                </StyleSelect>
+              </Wrapper>
+              {read}
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Write Contract" key="3">
+              <Wrapper>
+                <StyleSelect
+                  showSearch
+                  placeholder="Select a private key"
+                  allowClear
+                  onChange={this.onChange}
+                >
+                  {prikeys.prikeys && prikeys.prikeys.length !== 0
+                    ? prikeys.prikeys.map((value, index) => (
+                        <Option value={value.prikey} key={index}>
+                          {value.name}
+                        </Option>
+                      ))
+                    : null}
+                </StyleSelect>
+              </Wrapper>
+              {write}
+            </Tabs.TabPane>
+          </Tabs>
+        </Div>
+      </Wrapper>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		contract: state.contract,
-		prikeys:state.prikeyManagement,
-		login: state.login,
-	};
-};
+  return {
+    contract: state.contract,
+    prikeys: state.prikeyManagement,
+    login: state.login,
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     updatePriKey: (key) => {
-      dispatch(updatePriKey(key));
-    }
-  };
-};
+      dispatch(updatePriKey(key))
+    },
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(ContractCodeMenu);
+export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(
+  ContractCodeMenu
+)
--- src/components/contract/contractMenu.js
+++ src/components/contract/contractMenu.js
@@ -1,30 +1,30 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import {Tabs} from 'antd';
-import ContractCodeMenu from './contractCodeMenu';
-import TokenBalance from './tokenBalance';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Tabs } from 'antd'
+import ContractCodeMenu from './contractCodeMenu'
+import TokenBalance from './tokenBalance'
 
-class ContractMenu extends Component{
-	render(){
-		return (
-	        <Tabs defaultActiveKey="1" type="card">
-			    <Tabs.TabPane tab="TOKEN BALANCE" key="1">
-			        <TokenBalance/>
-			    </Tabs.TabPane>
-			    <Tabs.TabPane tab="CONTRACT" key="2">
-			    	<ContractCodeMenu/>
-			    </Tabs.TabPane>
-			</Tabs>
-		); 
-	}
+class ContractMenu extends Component {
+  render() {
+    return (
+      <Tabs defaultActiveKey="2" type="card">
+        <Tabs.TabPane tab="TOKEN BALANCE" key="1">
+          <TokenBalance />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="CONTRACT" key="2">
+          <ContractCodeMenu />
+        </Tabs.TabPane>
+      </Tabs>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-	};
-};
-const mapDispatchToProps = dispatch => {
-	return {
-	};
-};
-export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(ContractMenu);
+  return {}
+}
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(
+  ContractMenu
+)
